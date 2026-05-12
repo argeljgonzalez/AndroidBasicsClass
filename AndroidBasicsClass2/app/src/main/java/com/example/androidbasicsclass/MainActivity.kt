55c8d7f148d850830245ee65a5948fe0c9bf39d3
argeljgonzalez <argel.j.gonzalez@gmail.com>
2026-05-11 18:53:37 -0600

    appnavigation e incorporacion del mismo

--- AndroidBasicsClass2/app/src/main/java/com/example/androidbasicsclass/MainActivity.kt
+++ AndroidBasicsClass2/app/src/main/java/com/example/androidbasicsclass/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBasicsClassTheme {
-                AppNavigation()
+                com.example.androidbasicsclass.ui.navigation.AppNavigation()
             }
         }
     }
